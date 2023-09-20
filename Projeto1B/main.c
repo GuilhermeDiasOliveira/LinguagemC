533c188ea7eff89407dc559940c8f06a79faed8e
GuilhermeDiasOliveira <143664414+GuilhermeDiasOliveira@users.noreply.github.com>
2023-09-20 20:42:21 -0300

    Add files via upload

---
+++ Projeto1B/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("Foda-se\nAcontece isoo ai\n");
+	printf("Ricardo milos");
+	return 0;
+}
